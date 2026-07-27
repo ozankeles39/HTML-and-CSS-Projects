6d0a104c6ed8603cd12e99a39e04a20d757ec3a4
ozankeles39 <ozankelesca@gmail.com>
2026-07-26 23:56:06 -0400

    Add Academy Cinemas project with Bootstrap footer

---
+++ Hello_World.js
@@ -0,0 +1,7 @@
+var greeting1 = "Hello, World!";
+var greeting2 = "Welcome to my website.";
+var sentence1 = "This is my challenge file.";
+
+console.log(greeting1);
+console.log(greeting2);
+console.log(sentence1);
